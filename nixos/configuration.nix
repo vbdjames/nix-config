53879a2f4d52513ed2654da27f934140b4a02882
Doug James <vbdjames@gmail.com>
2025-11-08 18:28:02 -0500

    clean up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,14 +11,6 @@
       ./hardware-configuration.nix
     ];
 
-  # sops
-  # sops.defaultSopsFile = ./secrets/secrets.yaml;
-  # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  # sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-  # sops.age.generateKey = true;
-  # sops.secrets.example-key = {};
-  # sops.secrets."myservice/my_subdir/my_secret" = {};
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
