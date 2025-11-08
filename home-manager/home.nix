c8b3a3ec1aa43787fa3e0d27839d2968b477a481
Doug James <vbdjames@gmail.com>
2025-11-08 15:50:41 -0500

    add thunderbird email accounts

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,9 +6,68 @@
 
   home.packages = with pkgs; [
     neofetch
-    thunderbird
     obsidian
   ];
+
+  programs.thunderbird = {
+    enable = true;
+    profiles = {
+      default = {
+        isDefault = true;
+      };
+    };
+  };
+
+  accounts.email.accounts = {
+    "doug@dwjames.org" = {
+      primary = true;
+      address = "doug@dwjames.org";
+      flavor = "fastmail.com";
+      imap.host = "imap.fastmail.com";
+      realName = "Doug James";
+      smtp.host = "smtp.fastmail.com";
+      thunderbird = {
+        enable = true;
+        settings = id: {
+          "mail.server.server_${id}.authMethod" = 10;
+          "mail.smtpserver.smtp_${id}.authMethod" = 10;
+        };
+      };
+      userName = "doug@dwjames.org";
+    };
+    "vbdjames@gmail.com" = {
+      address = "vbdjames@gmail.com";
+      flavor = "gmail.com";
+      imap.host = "imap.gmail.com";
+      imap.port = 993;
+      realName = "Doug James";
+      smtp.host = "smtp.gmail.com";
+      smtp.port = 465;
+      thunderbird = {
+        enable = true;
+        settings = id: {
+          "mail.server.server_${id}.authMethod" = 10;
+          "mail.smtpserver.smtp_${id}.authMethod" = 10;
+        };
+      };
+    };
+    "doug.james@customviewbook.com" = {
+      address = "doug.james@customviewbook.com";
+      flavor = "gmail.com";
+      imap.host = "imap.gmail.com";
+      imap.port = 993;
+      realName = "Doug James";
+      smtp.host = "smtp.gmail.com";
+      smtp.port = 465;
+      thunderbird = {
+        enable = true;
+        settings = id: {
+          "mail.server.server_${id}.authMethod" = 10;
+          "mail.smtpserver.smtp_${id}.authMethod" = 10;
+        };
+      };
+    };
+  };
 
   home.stateVersion = "25.05";
 
